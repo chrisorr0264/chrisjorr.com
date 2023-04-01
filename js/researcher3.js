6f6c59f0ad22de6264302051fa4e8547222854e3
chrisorr0264 <127611920+chrisorr0264@users.noreply.github.com>
2023-04-01 05:12:52 -0700

    Update researcher3.js

--- js/researcher3.js
+++ js/researcher3.js
@@ -9,73 +9,73 @@ async function populate() {
 
     populateArticles(pubs);
 
-    }
+}
 
-    function populateArticles(obj) {
-        const section = document.querySelector("#pubCards");
-    
-        for (const pub of obj.pubs) {
-            if(pub.show == "main" || pub.show == "yes"){
-                const myArticle = document.createElement('div');
-                myArticle.classList.add("pub-list-item");
-                
-                const myI = document.createElement('i');
-                myI.classList.add("far", "fa-file-alt", "pub-icon");
-                myArticle.appendChild(myI);
+function populateArticles(obj) {
+    const section = document.querySelector("#pubCards");
 
-                const mySpanClass = document.createElement('span');
-                mySpanClass.classList.add("article-metadata", "li-cite-author");
-                myArticle.appendChild(mySpanClass);
+    for (const pub of obj.pubs) {
+        if(pub.show == "main" || pub.show == "yes"){
+            const myArticle = document.createElement('div');
+            myArticle.classList.add("pub-list-item");
+            
+            const myI = document.createElement('i');
+            myI.classList.add("far", "fa-file-alt", "pub-icon");
+            myArticle.appendChild(myI);
 
-                    var count = 0;
-                    const maxCount = pub.authors.length;
-                
-                    for(const author of pub.authors){
-                        
-                        count++;
-                        const mySpan = document.createElement('span');
-                        var boldName = author.name;
+            const mySpanClass = document.createElement('span');
+            mySpanClass.classList.add("article-metadata", "li-cite-author");
+            myArticle.appendChild(mySpanClass);
 
-                        if(count == maxCount){
-                            boldName = boldName + ".";
-                        } else {
-                            boldName = boldName + "; ";
-                        }
-                        if (boldName.includes("Orr")){
-                            const myBold = document.createElement('b');
-                            myBold.textContent = boldName;
-                            myArticle.appendChild(myBold);
-                        } else {
-                            mySpan.textContent = boldName;
-                        }
-                        
+                var count = 0;
+                const maxCount = pub.authors.length;
+            
+                for(const author of pub.authors){
+                    
+                    count++;
+                    const mySpan = document.createElement('span');
+                    var boldName = author.name;
 
-                        myArticle.appendChild(mySpan);
+                    if(count == maxCount){
+                        boldName = boldName + ".";
+                    } else {
+                        boldName = boldName + "; ";
                     }
-                const myYear = document.createElement('a');
-                myYear.textContent = " (" + pub.year + "). ";
-                myArticle.appendChild(myYear);
+                    if (boldName.includes("Orr")){
+                        const myBold = document.createElement('b');
+                        myBold.textContent = boldName;
+                        myArticle.appendChild(myBold);
+                    } else {
+                        mySpan.textContent = boldName;
+                    }
+                    
 
-                const myPub = document.createElement('a');
-                myPub.href = pub.article_link;
-                myPub.innerText = pub.article + " ";
-                myArticle.appendChild(myPub);
+                    myArticle.appendChild(mySpan);
+                }
+            const myYear = document.createElement('a');
+            myYear.textContent = " (" + pub.year + "). ";
+            myArticle.appendChild(myYear);
 
-                const myNewEm = document.createElement('em');
-                myNewEm.textContent = pub.journal;
-                myArticle.appendChild(myNewEm);
+            const myPub = document.createElement('a');
+            myPub.href = pub.article_link;
+            myPub.innerText = pub.article + " ";
+            myArticle.appendChild(myPub);
 
-                const myPara = document.createElement('p');
-                const myCite = myPara.appendChild(document.createElement('a'));
-                myCite.href = '#';
-                myCite.classList.add("btn", "btn-outline-primary", "btn-page-header", "btn-sm", "js-cite-modal");
-                myCite.setAttribute("data-filename",pub.cite_link);
-                myCite.innerText = "Cite";
-                myPara.appendChild(myCite);
-                myArticle.appendChild(myPara);
+            const myNewEm = document.createElement('em');
+            myNewEm.textContent = pub.journal;
+            myArticle.appendChild(myNewEm);
 
-                section.appendChild(myArticle);
-            }   
-        }
+            const myPara = document.createElement('p');
+            const myCite = myPara.appendChild(document.createElement('a'));
+            myCite.href = '#';
+            myCite.classList.add("btn", "btn-outline-primary", "btn-page-header", "btn-sm", "js-cite-modal");
+            myCite.setAttribute("data-filename",pub.cite_link);
+            myCite.innerText = "Cite";
+            myPara.appendChild(myCite);
+            myArticle.appendChild(myPara);
+
+            section.appendChild(myArticle);
+        }   
     }
+}
 
