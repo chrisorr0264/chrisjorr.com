b8de88d7482cbf85e684d8614cc77ba3df174612
chrisorr0264 <127611920+chrisorr0264@users.noreply.github.com>
2023-04-12 15:33:06 -0700

    update

--- js/researcher3.js
+++ js/researcher3.js
@@ -39,13 +39,9 @@ async function populateExp(){
 
 function populateArticles(obj) {
     const section = document.querySelector("#pubCards");
-
-
-
-
     alert(document.title.includes("publications"));
     for (const pub of obj.pubs) {
-        if((!document.title.includes("publications") && pub.show=="main2") || (document.title.includes("publications")&& ((pub.show=="main") || (pub.show=="yes")))){
+        if((!document.title.includes("publications") && pub.show=="main") || (document.title.includes("publications") && (pub.show=="main" || pub.show=="yes"))){
             const myArticle = document.createElement('div');
             myArticle.classList.add("pub-list-item");
             
