6acf716bb579b26426109b202d21a684a167851a
chrisorr0264 <127611920+chrisorr0264@users.noreply.github.com>
2023-04-15 14:27:39 -0700

    Update researcher3.js

--- js/researcher3.js
+++ js/researcher3.js
@@ -270,3 +270,12 @@ function populateKudos(obj) {
         }   
     }
 }
+/* Smooth Scrolling */
+document.getElementById('navigation').addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = e.target;
+    if (target.classList.contains('nav-link')) {
+        const id = target.getAttribute('href').slice(1);
+        document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+    }
+});
